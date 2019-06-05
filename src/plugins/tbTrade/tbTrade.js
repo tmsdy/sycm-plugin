fd31f5a7fb8ba3a5fc31a5cb6fb8934fb84e4417
zhlearn <zhlearn@sina.com>
2019-06-05 18:07:20 +0800

    0605   taobao trade

--- src/plugins/tbTrade/tbTrade.js
+++ src/plugins/tbTrade/tbTrade.js
@@ -1,1 +1,17 @@
-console.log('taobao 交易管理')
+console.log("taobao 交易管理");
+$(function () {
+    var haset = true;
+    $('#page').on('DOMNodeInserted', function (e) {
+        console.log(e.target.id, ',', e.target.className)
+        if (e.target.className == 'ww-inline ww-online') {
+            if (haset) {
+                $('.item-mod__trade-order___2LnGB .buyer-mod__buyer___3NRwJ').parent().append('<button>11</button>');
+                haset = false
+            }
+
+        }
+    })
+    $(document).on('click', '#sold_container .pagination li', function () {
+        haset = true;
+    })
+})
