a3a424bf87900864424d949cf021769b7cfb237a
zhlearn <zhlearn@sina.com>
2019-06-29 11:33:51 +0800

    local file

--- src/plugins/sycmModule/contentScript.js
+++ src/plugins/sycmModule/contentScript.js
@@ -6,62 +6,61 @@ window.SET_WAIT_TIME = 300000;
 // window.isLogin = false;
 // $(function () {
     // 获取线上资源
-    function loadCDNCss(cssUrl) {
-        chrome.runtime.sendMessage({
-            key: 'getData',
-                options: {
-                    url: cssUrl,
-                    type: 'GET'
-                }
-        }, function (res2) {
-            if (res2) {
-                var style = document.createElement('style');
-                style.type = 'text/css';
-                style.rel = 'stylesheet';
-                //for Chrome Firefox Opera Safari
-                style.appendChild(document.createTextNode(res2));
-                //for IE
-                //style.styleSheet.cssText = res2;
-                var head = document.getElementsByTagName('head')[0];
-                head.appendChild(style);
-            }
-        });
-    }
-
-    function loadCDNJs(jsUrl) {
-        chrome.runtime.sendMessage({
-           key: 'getData',
-            options: {
-                url: jsUrl,
-                type: 'GET'
-            }
-        }, function (cdnJS) {
-            eval(cdnJS);
-        })
-    }
-    // setTimeout(function () {
-        chrome.runtime.sendMessage({
-            key: 'getData',
-            options:{
-                url: BASE_URL +'/api/v1/plugin/getConfig',
-                type:'GET'
-            }
-        }, function (res) {
-            if (res.code == 200) {
-                CHAQZ_VERSION = res.data.version;
-                SET_WAIT_TIME = res.data.times*1000;
-                var cssurl = res.data.css.style;
-                var jsurl = res.data.js;
-                loadCDNCss(cssurl);
-                //   loadCDNJs('https://file.cdn.chaquanzhong.com/commons-dev-v1.0.41.js')
-                //   loadCDNJs('https://file.cdn.chaquanzhong.com/content_script1-dev-v1.0.41.js')
-                //   loadCDNJs('https://file.cdn.chaquanzhong.com/content_script-dev-v1.0.41.js')
-                for(var k in jsurl){
-                    if(k.indexOf('sycm')!=-1){
-                        loadCDNJs(jsurl[k])
-                    }
-                }
-            }
-        });
+    // function loadCDNCss(cssUrl) {
+    //     chrome.runtime.sendMessage({
+    //         key: 'getData',
+    //             options: {
+    //                 url: cssUrl,
+    //                 type: 'GET'
+    //             }
+    //     }, function (res2) {
+    //         if (res2) {
+    //             var style = document.createElement('style');
+    //             style.type = 'text/css';
+    //             style.rel = 'stylesheet';
+    //             //for Chrome Firefox Opera Safari
+    //             style.appendChild(document.createTextNode(res2));
+    //             //for IE
+    //             //style.styleSheet.cssText = res2;
+    //             var head = document.getElementsByTagName('head')[0];
+    //             head.appendChild(style);
+    //         }
+    //     });
+    // }
+    // function loadCDNJs(jsUrl) {
+    //     chrome.runtime.sendMessage({
+    //        key: 'getData',
+    //         options: {
+    //             url: jsUrl,
+    //             type: 'GET'
+    //         }
+    //     }, function (cdnJS) {
+    //         eval(cdnJS);
+    //     })
+    // }
+    // // setTimeout(function () {
+    //     chrome.runtime.sendMessage({
+    //         key: 'getData',
+    //         options:{
+    //             url: BASE_URL +'/api/v1/plugin/getConfig',
+    //             type:'GET'
+    //         }
+    //     }, function (res) {
+    //         if (res.code == 200) {
+    //             CHAQZ_VERSION = res.data.version;
+    //             SET_WAIT_TIME = res.data.times*1000;
+    //             var cssurl = res.data.css.style;
+    //             var jsurl = res.data.js;
+    //             loadCDNCss(cssurl);
+    //             //   loadCDNJs('https://file.cdn.chaquanzhong.com/commons-dev-v1.0.41.js')
+    //             //   loadCDNJs('https://file.cdn.chaquanzhong.com/content_script1-dev-v1.0.41.js')
+    //             //   loadCDNJs('https://file.cdn.chaquanzhong.com/content_script-dev-v1.0.41.js')
+    //             for(var k in jsurl){
+    //                 if(k.indexOf('sycm')!=-1){
+    //                     loadCDNJs(jsurl[k])
+    //                 }
+    //             }
+    //         }
+    //     });
     // }, 500);
 // // })
